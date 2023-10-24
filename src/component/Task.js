cfa0b685a5707e483d24c9b471c213daec920dfd
Sachin Chanduveetil <sachisavera@gmail.com>
2023-10-24 17:17:55 +0530

    first commit

---
+++ src/component/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Task = ({task , deleteTodo , index, handleEdit}) => {
+  return (
+    <div className='task'>
+    <h2> {task} </h2>
+    <button className='delete-btn' onClick={() => deleteTodo(index)}> Delete </button>
+    <button className='edit-btn' onClick={() => handleEdit(index)}> Edit </button>
+
+</div>
+  )
+}
+
+export default Task
---
+++ src/component/Todo.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Task from './Task';
+
+const Todo = () => {
+    const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  //adding Task
+  function AddTask() {
+    if (task !== "") {
+      setTaskList([...taskList, { task, index: `${task}-${Date.now()}` }]);
+      setTask('');
+    }
+
+    if (editId) {
+      const editTask = taskList.find((i) => i.index === editId);
+      const updatedTasks = taskList.map((t) => 
+        t.index === editTask.index ? t= {index: t.index , task} : {index: t.index, task: t.task}
+      )
+      setTaskList(updatedTasks);
+      setEditId(0);
+      return;
+    }
+  }
+
+  // deleting Todo .
+  const deleteTodo = (index) => {
+    var newList = taskList;
+    newList.splice(index, 1);
+    setTaskList([...newList]);
+  }
+
+  // Editing Task
+  const handleEdit = (index) => {
+    const editTask = taskList.find((i) => i.index === index);
+    setTask(editTask.task);
+    setEditId(index);
+  }
+
+  return (
+    <div className='todo-box'>
+      
+       <div className='todo-field'>
+       <h1 className='title'> Todo App </h1>
+       <input type='text' value={task} onChange={(e) => { setTask(e.target.value) }} />
+      <button onClick={AddTask}> Add </button>
+       </div>
+      
+
+      {taskList.map((task) => {
+        return <Task task={task.task} deleteTodo={deleteTodo} index={task.index} handleEdit={handleEdit} />
+      })}
+
+    </div>
+  )
+}
+
+export default Todo
